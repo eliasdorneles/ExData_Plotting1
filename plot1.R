6eb49e986603c9d1877dc9de8c5be026bfc55ad0
Elias Dorneles <eliasdorneles@gmail.com>
2014-05-11 00:12:28 -0300

    enforce size of png to be 480x480

--- plot1.R
+++ plot1.R
@@ -5,7 +5,7 @@
 # the zip file if needed, and extract only the data for those two dates
 raw_data <- read.csv("02-2007-household_power_consumption.csv", sep=";")
 
-png("plot1.png")
+png("plot1.png", width=480, height=480)
 hist(raw_data$Global_active_power, col='red', breaks=15,
      main="Global Active Power", xlab="Global Active Power (kilowatts)")
 dev.off()
